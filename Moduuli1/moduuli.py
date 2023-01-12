9af57c766b890a22c281f48ad989d44391fb31b5
Olli <olli.rounamo@metropolia.fi>
2023-01-12 10:30:41 +0200

    2

--- Moduuli1/moduuli.py
+++ Moduuli1/moduuli.py
@@ -1,3 +1,6 @@
-luku = int(input('Anna luku:'))
-luku = luku + 10
-print(luku)
+import math
+
+säde = float(input('Anna ympyrän säde sentteinä: '))
+pintaAla = math.pi * math.pow(säde, 2)
+
+print(f'Ympyrän, jonka säde on {säde:4.1f} pinta-ala on {pintaAla:5.1f}')
