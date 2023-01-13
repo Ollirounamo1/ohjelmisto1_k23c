7030e41963520960b452ac2bdd5ed1b40888ca7b
Olli <olli.rounamo@metropolia.fi>
2023-01-13 11:24:21 +0200

    tehtävä 3

---
+++ Moduuli1/t3.py
@@ -0,0 +1,8 @@
+a = int (input("Anna kanta: "))
+b = int (input("Anna korkeus: "))
+
+pintaAla=(a*b)
+piiri=(a+a+b+b)
+
+print("pintaAla on neliömetreinä", pintaAla)
+print("piiri on metreinä", piiri)
