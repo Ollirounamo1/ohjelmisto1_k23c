0eebcfe2dedc271446a98f3de57c04f64572617c
Olli <olli.rounamo@metropolia.fi>
2023-01-13 10:54:58 +0200

    tehtävä 4

---
+++ Moduuli1/t4.py
@@ -0,0 +1,13 @@
+a = int (input(" Anna ensimmäinen numero: "))
+
+b = int (input(" Anna toinen numero: "))
+
+c = int (input(" Anna kolmas numero: "))
+
+keskiarvo=(a+b+c)/3
+summa=(a+b+c)
+tulo=(a*b*c)
+
+print("Kolmen numeron keskiarvo on ", keskiarvo)
+print("kolmen numeron summa on", summa)
+print("kolmen numeron tulo on", tulo)
