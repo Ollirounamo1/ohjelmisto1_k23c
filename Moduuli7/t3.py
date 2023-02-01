de0966dbd70d091648e281f05e59d4f97de96a84
Olli <olli.rounamo@metropolia.fi>
2023-02-01 15:11:08 +0200

    tehtävä 3

--- Moduuli7/t3.py
+++ Moduuli7/t3.py
@@ -6,10 +6,12 @@ airports['EFHK'] = 'Helsinki vantaa'
 airports['EFVA'] = 'Vaasan lentokenttä'
 
 print(airports['EFVA'])
-#Jos toiminto on uusi
-    #Kysy icao
-    #kysy nimi
-    #lisää kenttä airports sanakirjaan
-#jos toiminto on haku
-    #kysy icao
-    #tulosta airports sanakirjasto alkio icao
+#toista jos toiminto ei ole ''
+    #Jos toiminto on uusi
+        #Kysy icao
+        #kysy nimi
+        #lisää kenttä airports sanakirjaan
+    #jos toiminto on haku
+        #kysy icao
+        #tulosta airports sanakirjasto alkio icao
+    #kysy toiminto
