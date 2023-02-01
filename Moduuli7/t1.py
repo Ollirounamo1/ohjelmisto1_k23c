d021f0992b601bc7981798af0d42dc5baa8bdf8b
Olli <olli.rounamo@metropolia.fi>
2023-02-01 14:36:21 +0200

    tehtävä 1

---
+++ Moduuli7/t1.py
@@ -0,0 +1,5 @@
+Kuukaudet = ('Talvi', 'Talvi', 'Talvi', 'Kevät', 'Kevät', 'Kevät', 'Kesä', 'Kesä', 'Kesä', 'Syksy', 'Syksy', 'Syksy')
+
+kuukausi = int(input("Anna kuukauden numero: "))
+
+print("Nyt on:", Kuukaudet[kuukausi - 1])
